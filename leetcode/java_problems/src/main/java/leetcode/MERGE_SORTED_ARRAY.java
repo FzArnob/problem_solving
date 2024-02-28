7f823ee683313963cd8c800776d206723b9b116a
Md. Farhan Zaman <farhanzamanarnob@gmail.com>
2024-02-28 16:08:14 +0600

    leetcode started

---
+++ leetcode/java_problems/src/main/java/leetcode/MERGE_SORTED_ARRAY.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import leetcode.merge_sorted_array.Solution;
+
+public class MERGE_SORTED_ARRAY {
+    public static void main(String[] args) {
+        // Example 1
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+
+        System.out.println("Before merging: ");
+        printArray(nums1);
+        printArray(nums2);
+
+        Solution solution = new Solution();
+        solution.merge(nums1, m, nums2, n);
+
+        System.out.println("After merging: ");
+        printArray(nums1);
+        printArray(nums2);
+    }
+
+    // Helper method to print an array
+    private static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ leetcode/java_problems/src/main/java/leetcode/merge_sorted_array/Solution.java
@@ -0,0 +1,17 @@
+package leetcode.merge_sorted_array;
+public class Solution {
+    // https://leetcode.com/problems/merge-sorted-array
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+}

d4314556967db867b34fce22d6b58f32438a5acd
Md. Farhan Zaman <farhanzamanarnob@gmail.com>
2024-03-01 18:00:12 +0600

    Create Solution.java

---
+++ leetcode/java_problems/src/main/java/leetcode/remove_duplicates_from_sorted_array_II/Solution.java
@@ -0,0 +1,13 @@
+package leetcode.remove_duplicates_from_sorted_array_II;
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (j == 1 || nums[i] != nums[j - 2]) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
+    }
+}
